b879f05d3c1f65a5387e287e264696e5df44baea
penolove <chinghua.yang@appier.com>
2019-10-21 01:02:35 +0800

    update implementation of Trie

--- leetcode/208/test_trie_implmentation.py
+++ leetcode/208/test_trie_implmentation.py
@@ -16,50 +16,48 @@ class Trie:
     are only associated with the leaves.
     It is also known as prefix tree as all descendants of a node have a common prefix of the string
     associated with that node, and the root is associated with the empty string.
+
+    Runtime: 128 ms, faster than 98.99% of Python3 online submissions
     """
     def __init__(self):
         """
         Initialize your data structure here.
         """
+        # use None as the finish point
+        self.end_up_key = None
         self.mapping = {}
-        self.located = False
 
     def insert(self, word: str) -> None:
         """
         Inserts a word into the trie.
         """
-        cursor = self
+        mapping = self.mapping
         for w in word:
-            mapping = cursor.mapping
-            if w in mapping:
-                cursor = mapping[w]
-            else:
-                mapping[w] = Trie()
-                cursor = mapping[w]
-        cursor.located = True
+            if w not in mapping:
+                mapping[w] = {}
+            mapping = mapping[w]
+        mapping[self.end_up_key] = True
 
     def search(self, word: str) -> bool:
         """
         Returns if the word is in the trie.
         """
-        cursor = self
+        mapping = self.mapping
         for w in word:
-            if w in cursor.mapping:
-                cursor = cursor.mapping[w]
-            else:
+            if w not in mapping:
                 return False
-        return cursor.located
+            mapping = mapping[w]
+        return mapping.get(self.end_up_key, False)
 
     def startsWith(self, prefix: str) -> bool:
         """
         Returns if there is any word in the trie that starts with the given prefix.
         """
-        cursor = self
+        mapping = self.mapping
         for w in prefix:
-            if w in cursor.mapping:
-                cursor = cursor.mapping[w]
-            else:
+            if w not in mapping:
                 return False
+            mapping = mapping[w]
         return True
 
 
